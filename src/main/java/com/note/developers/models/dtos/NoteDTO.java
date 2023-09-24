209a5defaadca4744501ec7c2be1ef045d0b349f
azizurrahman <azizur7543@gmail.com>
2023-09-24 13:04:51 +0530

    added creationtime in NoteDTO

--- src/main/java/com/note/developers/models/dtos/NoteDTO.java
+++ src/main/java/com/note/developers/models/dtos/NoteDTO.java
@@ -2,6 +2,9 @@ package com.note.developers.models.dtos;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Entity(name = "note")
 public class NoteDTO {
 
@@ -14,7 +17,17 @@ public class NoteDTO {
     public UserInfoDTO user;
 
 
+   @Column(name="title",nullable = false)
+   public String title;
+
+   @Column(name = "description", columnDefinition = "TEXT")
+   public String description;
+
+
    @Column(columnDefinition = "TEXT")
     public String code;
+
+   @Column(columnDefinition = "datetime default CURRENT_TIMESTAMP")
+   public LocalDateTime creationTime;
 
 }
